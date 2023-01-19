29437917167ec4fe09ef7844275fe340c492b31b
dinoguei <dinoguei@c1r8s13.42porto.com>
2023-01-19 14:11:08 +0000

    How to initialize the window

--- Understanding Minilbx/init.c
+++ Understanding Minilbx/init.c
@@ -19,6 +19,9 @@ int	main(void)
 	void	*mlx_win;
 
 	mlx = mlx_init();
+	//This will estabilish a connection to the graphical system,and returns a void * which holds the locaction of the MLX instance
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	//It will return a pointer to the window we created, we give the height, width and a title
 	mlx_loop(mlx);
+	//It will initiate the window rendering
 }
